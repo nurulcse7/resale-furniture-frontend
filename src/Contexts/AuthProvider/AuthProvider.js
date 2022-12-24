ca51e3e96485f76e4123b52fc81887d63151ca9c
nurulcse7 <nurul.cse7@gmail.com>
2022-12-24 08:38:44 +0600

    Added Hooks

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -12,8 +12,10 @@ import {
 import app from "../../firebase/firebase.config";
 import Loader from "../../Pages/Shared/Loader/Loader";
 export const AuthContext = createContext();
+export const CategoryContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+	const [category, setCategory] = useState('')
 	// user state
 	const [user, setUser] = useState(null);
 	// loading state
@@ -72,7 +74,11 @@ const AuthProvider = ({ children }) => {
 	return (
 		<div>
 			{loading && <Loader />}
-			<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+			<AuthContext.Provider value={authInfo}>
+				<CategoryContext.Provider value={{ category, setCategory }}>
+					{children}
+				</CategoryContext.Provider>
+			</AuthContext.Provider>
 		</div>
 	);
 };
