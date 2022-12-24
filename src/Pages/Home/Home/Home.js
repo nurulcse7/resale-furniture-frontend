a10fa933e348654c3862aa0070d78caf30b55ec2
nurulcse7 <nurul.cse7@gmail.com>
2022-12-24 09:51:42 +0600

    Updated Gallery

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useTitle from "../../../Hooks/useTitle";
+import Advertise from "../Advertise/Advertise";
 import Banner from "../Banner/Banner";
 import ContactUs from "../ContactUs/ContactUs";
 import Gallery from "../Gallery/Gallery";
@@ -10,6 +11,7 @@ const Home = () => {
 	return (
 		<div>
 			<Banner />
+			<Advertise />
 			<HomeCategories />
 			<Gallery />
 			<ContactUs />
